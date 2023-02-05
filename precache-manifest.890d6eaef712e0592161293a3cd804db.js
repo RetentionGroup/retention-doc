beaf59bc517b9d6d660c11e7a55ce062e69ace7a
Artem Babenko <artembabenko.work@gmail.com>
2023-02-05 14:39:25 +0200

    Updates

---
+++ precache-manifest.890d6eaef712e0592161293a3cd804db.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79e8095910d055f8a1755ef2c0a8f89f",
+    "url": "/retention-doc/index.html"
+  },
+  {
+    "revision": "0bc4aeb35e67742e2f82",
+    "url": "/retention-doc/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "1051e810c302d9a30117",
+    "url": "/retention-doc/static/css/main.c5761e8e.chunk.css"
+  },
+  {
+    "revision": "0bc4aeb35e67742e2f82",
+    "url": "/retention-doc/static/js/2.7143b53d.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/retention-doc/static/js/2.7143b53d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1051e810c302d9a30117",
+    "url": "/retention-doc/static/js/main.0a0e5eef.chunk.js"
+  },
+  {
+    "revision": "a01457852cc519fc3911",
+    "url": "/retention-doc/static/js/runtime-main.8a95b52f.js"
+  },
+  {
+    "revision": "17c97745d6d34ee6602b4151463f75dc",
+    "url": "/retention-doc/static/media/cat_song.17c97745.mp3"
+  }
+]);
